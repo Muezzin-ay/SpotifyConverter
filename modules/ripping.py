963c74046d6f05c72229eae2423e9fc827a213bc
HyperCaption <alex.faehndrich@gmail.com>
2022-04-15 19:12:10 +0200

    changed requirements back to original state

--- modules/ripping.py
+++ modules/ripping.py
@@ -3,9 +3,8 @@
 
 
 import yt_dlp
-"""Beatiful Soup for extracting finding the youtube url by name & interpreter"""
-from bs4 import BeautifulSoup
-import requests
+
+
 
 class get_url_using_name():
 
@@ -14,13 +13,9 @@ class get_url_using_name():
         self.name = name
 
     def find_by_artist_url(self):
-        #getting the request
-        raw_webpage = requests.get(self.youtube_url)
-
-        #converting the text
-        webpage = BeautifulSoup(raw_webpage.text, "html.parser")
-
-
+        pass
+    
+        
     def create_youtube_url(self):
         url_attachment = self.name.replace(" ","+")
         prefix = "https://www.youtube.com/results?search_query="
